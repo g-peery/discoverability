cc1a299b5331c0faebdbc2dea3d5338e860445a8
Gabriel Peery <gpeery@uoregon.edu>
2022-01-26 19:06:47 -0800

    Finished decompression, started parsing.

--- src/index.py
+++ src/index.py
@@ -4,9 +4,11 @@ index.py - Create a local cache of relevant manpage information.
 Author: Gabriel Peery
 Date: 1/25/2022
 """
+# BIG TODO - need way to be sure that files are manual pages
 import bz2
 import enum
 import gzip
+import locale
 import os
 import subprocess
 from typing import List
@@ -20,37 +22,37 @@ class DependencyNotFoundError(Exception):
     pass
 
 
-class CompressionType(enum.Enum):
+class CompressT(enum.Enum):
+    NONE = enum.auto()
     GZIP = enum.auto()
     BZIP2 = enum.auto()
 
 
-# TODO - use bz2, gzip packages to open them up. For debug, we'll first
-# inspect how many files are in the archives.
 class ManualPage:
     """Object containing information on a manual page."""
 
     # Magic bytes
     _TYPE_LOOKUP = {
-    b"\x1f\x8b" : CompressionType.GZIP,
-    b"BZ" : CompressionType.BZIP2
+    b"\x1f\x8b" : CompressT.GZIP,
+    b"BZ" : CompressT.BZIP2
     }
 
-    def __init__(self, path : str):
+    def __init__(self, path : str, locale : str):
         """
-        A manual page object constructed from analyzing the file at
-        the given path.
+        A manual page object constructed from analyzing the file at the 
+        given path. Will decompress if needed, then handle according to 
+        provided locale.
         """
         self._paths = set()
+        self._locale = locale
+
         self.record_path(path)
-        #self._extract_info(path)
+        self._extract_info(path)
 
     def __str__(self) -> str:
         """str(self) - Pretty printed string version"""
         return f"""Paths: {str(self._paths)}
-Body:(
-{self.main_text}
-)"""
+Title:{self._title}"""
 
     def _extract_info(self, path : str):
         """Read info from file to this object."""
@@ -59,11 +61,11 @@ Body:(
             # Determine File Type
             # 
             # Check for magic characters
-            try:
-                compression_type = self._TYPE_LOOKUP[file_obj.read(2)]
-            except KeyError:
-                # TODO - raise appropriate exception for not a good file
-                pass
+            compress_t = self._TYPE_LOOKUP.setdefault(
+                file_obj.read(2),
+                CompressT.NONE
+            )
+
             # Go back to start
             if file_obj.seekable():
                 file_obj.seek(0)
@@ -74,7 +76,55 @@ Body:(
             # 
             # Extract information
             #
-            self.main_text = file_obj.read()
+            # Get appropriate file object
+            if compress_t == CompressT.GZIP:
+                zipped_file = gzip.GzipFile(fileobj=file_obj)
+            elif compress_t == CompressT.BZIP2:
+                zipped_file = bz2.BZ2File(file_obj)
+            else:
+                zipped_file = file_obj
+
+            # Read plaintext
+            full_text = zipped_file.read().decode(self._locale, "replace")
+
+            # Close wrapper file object as appropriate
+            if compress_t == CompressT.GZIP or compress_t == CompressT.BZIP2:
+                zipped_file.close()
+
+        # Finally, parse through file
+        self._parse(full_text)
+
+    def _parse(self, full_text : str):
+        """Set local variables to store relevant information on page."""
+        lines = full_text.splitlines()
+        lines_iterator = iter(lines)
+
+        # Find man page title
+        self._title = None
+        try:
+            while self._title is None:
+                line = next(lines_iterator)
+                if line[:3] == ".TH":
+                    # This is the title line. Find the title field.
+                    stop_char = ' ' # Default space
+                    start_pos = 4
+                    if line[4] == '"':
+                        # Case go in between the quotes
+                        stop_char = '"'
+                        start_pos += 1
+
+                    # Build title character by character
+                    self._title = ""
+                    for idx, c in enumerate(line[start_pos:]):
+                        if c == stop_char:
+                            break
+                        if c == '\\' and not line[start_pos + idx - 1] == '\\':
+                            continue
+                        self._title += c
+                        
+        except StopIteration:
+            # TODO - throw an error that this is not a manpage.
+            pass
 
     def record_path(self, path : str):
         """Updates object record of paths seen."""
@@ -111,6 +161,9 @@ def index(paths : List[str], verbose : bool=False):
 
     Note that bad things will happen if there are link loops.
     """
+    # This will be needed to parse pages
+    encoding = locale.getdefaultlocale()[1]
+
     # Prepare pretty printing
     if verbose:
         try:
@@ -129,12 +182,14 @@ def index(paths : List[str], verbose : bool=False):
                 # Check if already seen
                 if real_path not in pages:
                     # First time analysis
-                    page = ManualPage(real_path)
+                    if verbose:
+                        print(real_path.center(term_size, "-"))
+
+                    page = ManualPage(real_path, encoding)
                     pages[real_path] = page
 
                     if verbose:
-                        print(real_path.center(term_size, "-"))
-                        #print(page)
+                        print(page)
                 else:
                     # nth time analysis.
                     pages[real_path].record_path(full_path)
